a02ca1b4c571e4161b70c4677fe73384f217622e
spurgason <shanepurgason.98@gmail.com>
2021-08-31 19:40:54 -0400

    Added Bootstrap Rows
    -added rows and columns for time slots

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,5 @@
-console.log('hello')
+// Displays the current date using moment.js
+const todaysDate = document.getElementById('currentDay')
+let today = moment().format('MMM Do YYYY')
+todaysDate.innerHTML = "Today's date is " + today
+

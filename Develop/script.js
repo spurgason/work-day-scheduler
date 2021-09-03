72f53fb0a84d072652292f86d800eb5b90bdac41
spurgason <shanepurgason.98@gmail.com>
2021-09-03 15:47:09 -0400

    Got local storage working

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,30 @@ $('#currentDay').html(today)
 let currentHour = moment().hour();
 console.log(currentHour);
 
+// Local Storage Save 
+$('.saveBtn').on('click', function(){
+
+    let task = $(this).siblings("#textBlock").val();
+    let hour = $(this).parent().attr("id");
+
+    localStorage.setItem(hour, task)
+})
+
+// Gets local storage through key displaying the tasks discription
+$("#hour8AM #textBlock").val(localStorage.getItem("hour8AM"));
+$("#hour9AM #textBlock").val(localStorage.getItem("hour9AM"));
+$("#hour10AM #textBlock").val(localStorage.getItem("hour10AM"));
+$("#hour11AM #textBlock").val(localStorage.getItem("hour11AM"));
+$("#hour12PM #textBlock").val(localStorage.getItem("hour12PM"));
+$("#hour1PM #textBlock").val(localStorage.getItem("hour1PM"));
+$("#hour2PM #textBlock").val(localStorage.getItem("hour2PM"));
+$("#hour3PM #textBlock").val(localStorage.getItem("hour3PM"));
+$("#hour4PM #textBlock").val(localStorage.getItem("hour4PM"));
+$("#hour5PM #textBlock").val(localStorage.getItem("hour5PM"));
+
+
+
+
 // Creates values for each time slot
 const hour8 = document.getElementById('hour-8').value = 8
 const hour9 = document.getElementById('hour-9').value = 9
@@ -31,29 +55,28 @@ let hourBlocks = [
     hour4,
     hour5]
 
-
-
 // Runs function to check if it is past, present, or future
 function pastToFuture() {
 
     // For loop to check each time in array
-   hourBlocks.forEach(function(checkTime){
+    hourBlocks.forEach(function (checkTime) {
 
-    // Sets the background color class for past, present, or future
-    if (hourBlocks < currentHour) {
-        $('#textBlock').addClass('past')
-        $('#textBlock').removeClass('present')
-        $('#textBlock').removeClass('future')
-    } else if (hourBlocks == currentHour) {
-        $('#textBlock').removeClass('past')
-        $('#textBlock').addClass('present')
-        $('#textBlock').removeClass('future')
-    } else {
-        $('#textBlock').removeClass('past')
-        $('#textBlock').removeClass('present')
-        $('#textBlock').addClass('future')
-    } 
-    console.log(checkTime);
-   })
-    
+        // Sets the background color class for past, present, or future
+        if (hourBlocks < currentHour) {
+            $('.description').addClass('past')
+            $('.description').removeClass('present')
+            $('.description').removeClass('future')
+        } else if (hourBlocks == currentHour) {
+            $('.description').removeClass('past')
+            $('.description').addClass('present')
+            $('.description').removeClass('future')
+        } else {
+            $('.description').removeClass('past')
+            $('.description').removeClass('present')
+            $('.description').addClass('future')
+        }
+        console.log(checkTime);
+    })
+
 } pastToFuture();
+

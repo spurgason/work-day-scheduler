7b72047d0ea1ea9d2761561f134f6a3c063c1989
spurgason <shanepurgason.98@gmail.com>
2022-01-02 16:40:41 -0500

    fixed time/color bug, updated readme

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,105 +1,44 @@
 // Displays the current date using moment.js
-let today = moment().format('MMM Do YYYY');
-$('#currentDay').html(today)
+let today = moment().format("MMM Do YYYY");
+$("#currentDay").html(today);
 
-// Sets variable for current time
-let currentHour = moment().hour();
-console.log(currentHour);
+// Local Storage Save
+$(".saveBtn").on("click", function () {
+  let task = $(this).siblings("#textBlock").val();
+  let hour = $(this).parent().attr("id");
 
-// Local Storage Save 
-$('.saveBtn').on('click', function(){
-
-    let task = $(this).siblings("#textBlock").val();
-    let hour = $(this).parent().attr("id");
-
-    localStorage.setItem(hour, task)
-})
-
-// Gets local storage through key displaying the tasks discription
-$("#hour8AM #textBlock").val(localStorage.getItem("hour8AM"));
-$("#hour9AM #textBlock").val(localStorage.getItem("hour9AM"));
-$("#hour10AM #textBlock").val(localStorage.getItem("hour10AM"));
-$("#hour11AM #textBlock").val(localStorage.getItem("hour11AM"));
-$("#hour12PM #textBlock").val(localStorage.getItem("hour12PM"));
-$("#hour1PM #textBlock").val(localStorage.getItem("hour1PM"));
-$("#hour2PM #textBlock").val(localStorage.getItem("hour2PM"));
-$("#hour3PM #textBlock").val(localStorage.getItem("hour3PM"));
-$("#hour4PM #textBlock").val(localStorage.getItem("hour4PM"));
-$("#hour5PM #textBlock").val(localStorage.getItem("hour5PM"));
-
-
-
-
-// Creates values for each time slot
-const hour8 = document.getElementById('hour-8').value = 8
-const hour9 = document.getElementById('hour-9').value = 9
-const hour10 = document.getElementById('hour-10').value = 10
-const hour11 = document.getElementById('hour-11').value = 11
-const hour12 = document.getElementById('hour-12').value = 12
-const hour1 = document.getElementById('hour-1').value = 13
-const hour2 = document.getElementById('hour-2').value = 14
-const hour3 = document.getElementById('hour-3').value = 15
-const hour4 = document.getElementById('hour-4').value = 16
-const hour5 = document.getElementById('hour-5').value = 17
-
-// Stored values for time slots
-// let hourBlocks = [
-//     hour8,
-//     hour9,
-//     hour10,
-//     hour11,
-//     hour12,
-//     hour1,
-//     hour2,
-//     hour3,
-//     hour4,
-//     hour5]
+  localStorage.setItem(hour, task);
+});
 
 // Runs function to check if it is past, present, or future
 function pastToFuture() {
+  // Sets variable for current time
+  let currentHour = moment().hours();
+  console.log(currentHour);
 
+  $(".time-block").each(function () {
+    var hourBlocks = parseInt($(this).attr("id").split("hour")[1]);
 
-    $('.time-block').each( function(){
-        var hourBlocks = parseInt($(this).attr("id").split("hour")[1]);
+    if (hourBlocks < currentHour) {
+      $(this).addClass("past");
+    } else if (hourBlocks == currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+}
 
-        if (hourBlocks < currentHour) {
-            $(this).addClass('past')
-            $(this).removeClass('present')
-            $(this).removeClass('future')
-        } else if (hourBlocks === currentHour) {
-            $(this).removeClass('past')
-            $(this).addClass('present')
-            $(this).removeClass('future')
-        } else {
-            $(this).removeClass('past')
-            $(this).removeClass('present')
-            $(this).addClass('future')
-        }
+pastToFuture();
 
-
-    })
-
-    // WHY DOES THIS FOR LOOP NOT WORK?
-    // For loop to check each time in array
-    // for (let index = 0; index < hourBlocks.length; index++) {
-
-        // Sets the background color class for past, present, or future
-        // if (hourBlocks < currentHour) {
-        //     $('.description').addClass('past')
-        //     $('.description').removeClass('present')
-        //     $('.description').removeClass('future')
-        // } else if (hourBlocks === currentHour) {
-        //     $('.description').removeClass('past')
-        //     $('.description').addClass('present')
-        //     $('.description').removeClass('future')
-        // } else {
-        //     $('.description').removeClass('past')
-        //     $('.description').removeClass('present')
-        //     $('.description').addClass('future')
-        // }
-        
-    // }
-    
-
-} pastToFuture();
-
+// Gets local storage through key displaying the tasks description
+$("#hour8 #textBlock").val(localStorage.getItem("hour8"));
+$("#hour9 #textBlock").val(localStorage.getItem("hour9"));
+$("#hour10 #textBlock").val(localStorage.getItem("hour10"));
+$("#hour11 #textBlock").val(localStorage.getItem("hour11"));
+$("#hour12 #textBlock").val(localStorage.getItem("hour12"));
+$("#hour13 #textBlock").val(localStorage.getItem("hour13"));
+$("#hour14 #textBlock").val(localStorage.getItem("hour14"));
+$("#hour15 #textBlock").val(localStorage.getItem("hour15"));
+$("#hour16 #textBlock").val(localStorage.getItem("hour16"));
+$("#hour17 #textBlock").val(localStorage.getItem("hour17"));
--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Work Day Scheduler
-This challenge required me to create a scheduler for hours of the work day. Depending on the hour of the day the background will change. 
+This applications purpose is to help individuals working a 9-5 job keep track of their daily tasks.
+![work-day-planner-example](Assets/Images/work-day.png)
 
-# <a href="https://spurgason.github.io/work-day-scheduler/">Deployed Application</a>
+## Technologies
+`HTML/CSS`<br>
+`Javascript`<br>
+`Moment.js`<br>
+`jQuery`<br>
 
-<img src="Assets/Images/work-day.png">
+## Install
+To install this application simply clone this repository, open it in your code editor and open the `index.html` in your browser.
+
+## Usage 
+This application allows users to save tasks to an hourly time slot. Depending on the hour the time slot's color will change indicating that the task is currently due, upcoming in the near future, or it has already passed.  
+
+## Live Application 
+[Here](https://spurgason.github.io/work-day-scheduler/)
+
+## Contributor
+[Shane Purgason](https://github.com/spurgason)
+
+## Questions
+[Github](https://github.com/spurgason) <br>
+[Email](mailto:shanepurgason.98@gmail.com)
